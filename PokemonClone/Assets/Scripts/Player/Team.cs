fb51e1129ff6d315e412cd4574c0817746c42d65
Mads Knudsen <madsfknudsen@hotmail.com>
2020-10-26 07:49:13 +0100

    +

---
+++ PokemonClone/Assets/Scripts/Player/Team.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team : MonoBehaviour
+{
+    List<Pokemon> currentTeam = new List<Pokemon>();
+}
---
+++ PokemonClone/Assets/Scripts/Pokemons/Pokemon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region Enums
+public enum PokemonState { EnterBattle, Idle, Attacking }
+#endregion
+
+public class Pokemon : MonoBehaviour, IPokemon
+{
+    #region Values:
+    [Header("Object Reference:")]
+    public PokemonState state;
+
+    [Header(" - Stats:")]
+    [SerializeField] protected float health;
+    [SerializeField] protected float defence, specialDefence;
+    [SerializeField] protected float attack, specialAttack;
+    #endregion
+
+    public void UpdatePokemon()
+    {
+        switch (state)
+        {
+            case PokemonState.Idle:
+                break;
+
+            case PokemonState.Attacking:
+                break;
+        }
+    }
+}
